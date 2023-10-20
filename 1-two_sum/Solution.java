39267b2855a3237c80dbd5e21cac16696316df0d
Isaac Medeiros <isaacoelhomedeiros@gmail.com>
2023-10-20 06:33:06 -0300

    uncommenting

--- 1-two_sum/Solution.java
+++ 1-two_sum/Solution.java
@@ -2,17 +2,17 @@ class Solution {
     public int[] twoSum(int[] nums, int target) {
         int[] indices = new int[2];
 
-        // for (int i = 0; i < nums.length - 1; i++) {
-        //     int aux = target - nums[i];
+         for (int i = 0; i < nums.length - 1; i++) {
+             int aux = target - nums[i];
 
-        //     for (int j = i + 1; j < nums.length; j++) {
-        //         if (aux == nums[j]) {
-        //             indices[0] = i;
-        //             indices[1] = j;
-        //             break;
-        //         }
-        //     }
-        // }
+             for (int j = i + 1; j < nums.length; j++) {
+                 if (aux == nums[j]) {
+                     indices[0] = i;
+                     indices[1] = j;
+                     break;
+                 }
+             }
+         }
 
         return indices;
     }
